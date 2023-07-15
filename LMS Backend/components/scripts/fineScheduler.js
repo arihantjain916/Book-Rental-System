aa45b22d8a1d130e1f1279f8eadc6d6896686322
Arihant Jain <81769948+arihantjain916@users.noreply.github.com>
2023-07-15 13:24:11 +0530

    Optimize the code

--- LMS Backend/components/scripts/fineScheduler.js
+++ LMS Backend/components/scripts/fineScheduler.js
@@ -4,31 +4,27 @@ const Rent = require('../models/RentBook');
 const updateFineForOverdueRentals = async () => {
     const currentDate = new Date();
 
-    const overdueRentals = await Rent.find({
-        returndate: { $lt: currentDate },
+    const overdueRentals = await Rent.findOne({
         books: { $elemMatch: { returned: false } },
     });
 
-    const filteredRentals = overdueRentals.filter((rental) => {
-        return rental.books.some((book) => !book.returned);
-    });
-
-    filteredRentals.forEach(async (rental) => {
-        for (const book of rental.books) {
-            if (!book.returned) {
+    if (overdueRentals) {
+        const filteredRentals = await overdueRentals.books.map((book) => {
+            if (!book.returned && new Date(book.returndate) < currentDate) {
                 const returnDate = new Date(book.returndate);
                 const daysLate = Math.floor((currentDate - returnDate) / (24 * 60 * 60 * 1000));
                 const fine = daysLate * 10;
 
-                book.fine = Math.min(fine, 1500);
-                book.returned = true;
+                book.fine = fine;
             }
-        }
 
-        await rental.save();
-    });
+            return book;
+        });
 
-    console.log('Fine updated for overdue rentals.');
+        await overdueRentals.save();
+
+        console.log('Fine updated for overdue rentals.');
+    }
 };
 
 const job = schedule.scheduleJob('0 0 * * *', updateFineForOverdueRentals);
